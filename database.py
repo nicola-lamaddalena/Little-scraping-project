6a3a14b5aeec46762895ace14fbb7c348d8866db
Nicola Lamaddalena <93214394+Uomonero1@users.noreply.github.com>
2022-10-15 12:58:48 +0200

    Update database.py

--- database.py
+++ database.py
@@ -5,6 +5,11 @@ import sqlite3
 conn = sqlite3.connect("name_of_the_database.db")
 cursor = conn.cursor()
 
+cursor.execute("DROP TABLE IF EXISTS auto")
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS auto(name char(50), price real, link char(100))
+  CREATE TABLE IF NOT EXISTS auto(
+  name char(50) PRIMARY KEY, 
+  price integer, 
+  link char(100)
+  )
 """)
